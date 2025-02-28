21f2b0fb74bf6dbd919fe5f6b5859af190780516
Hari HK <162097755+Harihk082005@users.noreply.github.com>
2025-02-28 12:08:27 +0530

    Update calc.java by main

--- calc.java
+++ calc.java
@@ -1,11 +1,2 @@
-public class Calc {
-    public int add(int a, int b) { return a + b; }
-    public int subtract(int a, int b) { return a - b; }
-
-    public static void main(String[] args) {
-        Calc calculator = new Calc();
-        System.out.println(calculator.add(5, 3));
-        System.out.println(calculator.subtract(5, 3)); 
-    }
-}
+hlo
 
